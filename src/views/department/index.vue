db82d51d5560f703009b4a44abd1bd8428adcd82
liu1428 <liuyitong2416@qq.com>
2025-11-13 10:28:14 +0800

    自定义结构，获取插槽数据

--- src/views/department/index.vue
+++ src/views/department/index.vue
@@ -2,11 +2,84 @@
   <div class="container">
     <div class="app-container">
       组织架构
+      <el-tree :data="treeData" default-expand-all props="default-props">
+        <!-- 节点结构 -->
+        <template v-slot="{data}">
+          <el-row type="flex" justify="space-between" align="middle" style="width: 100%;height: 40px;">
+            <el-col>{{ data.name }}</el-col>
+            <el-col :span="4">
+              <span class="trea-manager">{{ data.managerName }}</span>
+              <el-dropdown>
+                <span class="el-dropdown-link">操作 <i class="el-icon-arrow-down" /> </span>
+                <el-dropdown-menu slot="dropdown">
+                  <el-dropdown-item>添加子部门</el-dropdown-item>
+                  <el-dropdown-item>编辑部门</el-dropdown-item>
+                  <el-dropdown-item>删除</el-dropdown-item>
+                </el-dropdown-menu>
+              </el-dropdown>
+            </el-col>
+          </el-row>
+        </template>
+
+      </el-tree>
     </div>
   </div>
 </template>
 <script>
 export default {
-  name: 'Department'
+  name: 'Department',
+  data() {
+    return {
+      treeData: [
+        {
+          name: '桓狗教育',
+          managerName: '管理员',
+          children: [
+            {
+              name: '桓狗办',
+              managerName: '桓狗',
+
+              children: [{
+                name: '刘测试1',
+                managerName: '张三'
+
+              }, {
+                name: '刘测试2',
+                managerName: '李四'
+
+              }
+
+              ]
+            }, {
+              name: '行政部',
+              managerName: '王五'
+
+            }, {
+              name: '人事部',
+              managerName: '刘测试'
+
+            }
+          ]
+        }
+      ],
+      defaultProps: {
+        children: 'children',
+        label: 'name'
+      }
+    }
+  }
+
 }
 </script>
+
+<style scoped>
+.app-container{
+  padding: 30px 140px;
+  font-size: 14px;
+}
+.trea-manager{
+  width: 50px;
+  display: inline-block;
+  margin: 10px;
+}
+</style>
